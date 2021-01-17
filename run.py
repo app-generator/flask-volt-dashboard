80ea189431a34218b62c8d230cb3480c9eefbbe2
App Generator <support@appseed.us>
2021-01-17 10:26:51 +0200

    Bump Codebase & CSS CleanUP

--- run.py
+++ run.py
@@ -7,12 +7,13 @@ from flask_migrate import Migrate
 from os import environ
 from sys import exit
 from decouple import config
+import logging
 
 from config import config_dict
 from app import create_app, db
 
 # WARNING: Don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 # The configuration
 get_config_mode = 'Debug' if DEBUG else 'Production'
@@ -27,6 +28,11 @@ except KeyError:
 
 app = create_app( app_config ) 
 Migrate(app, db)
+
+if DEBUG:
+    app.logger.info('DEBUG       = ' + str(DEBUG)      )
+    app.logger.info('Environment = ' + get_config_mode )
+    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI )
 
 if __name__ == "__main__":
     app.run()
