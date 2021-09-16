938b20187d5e7842012eca77a32f4ad6817dd27c
App Generator <support@appseed.us>
2021-09-16 09:28:48 +0300

    Bump Codebase Version

--- run.py
+++ run.py
@@ -4,13 +4,11 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from flask_migrate import Migrate
-from os import environ
 from sys import exit
 from decouple import config
-import logging
 
-from config import config_dict
-from app import create_app, db
+from apps.config import config_dict
+from apps import create_app, db
 
 # WARNING: Don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
@@ -19,20 +17,20 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 get_config_mode = 'Debug' if DEBUG else 'Production'
 
 try:
-    
-    # Load the configuration using the default values 
+
+    # Load the configuration using the default values
     app_config = config_dict[get_config_mode.capitalize()]
 
 except KeyError:
     exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
 
-app = create_app( app_config ) 
+app = create_app(app_config)
 Migrate(app, db)
 
 if DEBUG:
-    app.logger.info('DEBUG       = ' + str(DEBUG)      )
-    app.logger.info('Environment = ' + get_config_mode )
-    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI )
+    app.logger.info('DEBUG       = ' + str(DEBUG))
+    app.logger.info('Environment = ' + get_config_mode)
+    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
 
 if __name__ == "__main__":
     app.run()
