1890599ee5722d06fb2bc0c6c96297e7e6104b39
abumafrim <abumafrim@gmail.com>
2022-06-27 10:33:42 +0100

    Update routes.py

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -20,28 +20,44 @@ def dashboard():
   if request.method == 'POST':
 
     records = []
+    line = ['source lang.', 'target lang.', 'text', 'translation', 'reviewed', 'comment']
 
     if request.form.get('allDownload'):
       records = Reviews.query.all()
 
+      return Response(getRecords(line, records, ''), mimetype="text/csv", headers={"Content-Disposition":"attachment;filename=reviews.csv"})
+
     if request.form.get('filterDownload'):
-      _start_date = request.form['startdate']
-      _end_date = request.form['enddate']
-      records = Reviews.query.filter(Reviews.reviewed_at.between(datetime.strptime(_start_date, '%d-%m-%Y'), datetime.strptime(_end_date, '%d-%m-%Y')))
 
-    output = io.StringIO()
-    writer = csv.writer(output)
+      if request.form.get('daterange'):
 
-    line = ['source lang.', 'target lang.', 'text', 'translation', 'reviewed', 'comment']
-    writer.writerow(line)
+        _start_date, _end_date = getDateRange()
 
-    for row in records:
-      line = [str(row.sl), str(row.tl), str(row.text), str(row.translation), str(row.reviewed), str(row.comment)]
-      writer.writerow(line)
+        records = filterDate(_start_date, _end_date)
+        dtype = 'daterange'
 
-    output.seek(0)
+        return Response(getRecords(line, records, dtype), mimetype="text/csv", headers={"Content-Disposition":"attachment;filename=reviews.csv"})
 
-    return Response(output, mimetype="text/csv", headers={"Content-Disposition":"attachment;filename=reviews.csv"})
+      if request.form.get('langpair'):
+        
+        _slang, _tlang = getLangPair()
+
+        records = filterLang(_slang, _tlang)
+        line = ['text', 'translation', 'reviewed', 'comment']
+        dtype = 'langpair'
+
+        return Response(getRecords(line, records, dtype), mimetype="text/csv", headers={"Content-Disposition":"attachment;filename=reviews.csv"})
+
+      if request.form.get('langpair') and request.form.get('daterange'):
+
+        _slang, _tlang = getLangPair()
+        _start_date, _end_date = getDateRange()
+
+        records = filterLangDate(_slang, _tlang, _start_date, _end_date)
+        line = ['text', 'translation', 'reviewed', 'comment']
+        dtype = 'langpair'
+
+        return Response(getRecords(line, records, dtype), mimetype="text/csv", headers={"Content-Disposition":"attachment;filename=reviews.csv"})
 
   per_page = 10
   start = 1 + (page - 1) * per_page
@@ -96,3 +112,50 @@ def get_segment(request):
 
   except:
     return None
+
+def filterDate(_start_date, _end_date):
+
+  return Reviews.query.filter(Reviews.reviewed_at.between(_start_date, _end_date))
+
+def filterLang(_sl, _tl):
+  
+  return Reviews.query.filter_by(sl=_sl, tl=_tl).all()
+
+def filterLangDate(_sl, _tl, _start_date, _end_date):
+  
+  return Reviews.query.filter_by(sl=_sl, tl=_tl).filter(Reviews.reviewed_at.between(_start_date, _end_date)).all()
+
+def getLangPair():
+
+  lcode = {"1" : ['es', 'Ndyuka (Aukaans)'], "2" : ['en', 'English'], "3" : ['fr', 'French']}
+  _slang = lcode.get(request.form['slang'])[1]
+  _tlang = lcode.get(request.form['tlang'])[1]
+
+  return _slang, _tlang
+
+def getDateRange():
+  
+  _start_date = datetime.strptime(request.form['startdate'], "%m/%d/%Y").strftime("%Y-%m-%d")
+  _end_date = datetime.strptime(request.form['enddate'], "%m/%d/%Y").strftime("%Y-%m-%d")
+
+  return _start_date, _end_date
+
+def getRecords(line, records, dtype):
+
+  output = io.StringIO()
+  writer = csv.writer(output)
+
+  writer.writerow(line)
+
+  for row in records:
+    if dtype == 'daterange' or dtype == '':
+      line = [str(row.sl), str(row.tl), str(row.text), str(row.translation), str(row.reviewed), str(row.comment)]
+    
+    elif dtype == 'langpair':
+      line = [str(row.text), str(row.translation), str(row.reviewed), str(row.comment)]
+    
+    writer.writerow(line)
+
+  output.seek(0)
+
+  return output
